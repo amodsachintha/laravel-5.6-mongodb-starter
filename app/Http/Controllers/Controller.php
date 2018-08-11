ea84ba46b0ce657f4ba48f0af18eeddaace627c4
amodsachintha <amod077@gmail.com>
2018-08-11 21:34:50 +0530

    init

---
+++ app/Http/Controllers/Controller.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Foundation\Bus\DispatchesJobs;
+use Illuminate\Routing\Controller as BaseController;
+use Illuminate\Foundation\Validation\ValidatesRequests;
+use Illuminate\Foundation\Auth\Access\AuthorizesRequests;
+use Illuminate\Support\Facades\DB;
+
+class Controller extends BaseController
+{
+    use AuthorizesRequests, DispatchesJobs, ValidatesRequests;
+
+
+
+    public function test(){
+        $users = DB::collection('users')->get();
+        return response()->json($users);
+
+    }
+
+
+
+
+
+
+
+
+}
